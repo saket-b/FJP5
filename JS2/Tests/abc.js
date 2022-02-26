e93fb756a852dbd65ec67decb156d60e76c12e2c
saket bharti <saket@tidalwave.tech>
2022-02-26 23:50:20 +0530

    test discussion

---
+++ JS2/Tests/abc.js
@@ -0,0 +1,21 @@
+
+
+// var a = 10;
+
+
+// console.log(a == "10");// auto matic type converter
+// console.log(typeof a);
+// console.log(typeof a === "number");
+// console.log(a === 10);
+
+
+// < --------------Question 1----------------->
+const emojis = ['🎄', '🎅🏼', '🎁', '⭐'];
+
+/* 1 */ emojis.push('🦌');
+/* 2 */ emojis.splice(0, 2);
+/* 3 */ var s =[...emojis, '🥂'];
+//emojis.length = 0;
+//console.log(emojis.length);
+console.log(emojis);
+console.log(s);
