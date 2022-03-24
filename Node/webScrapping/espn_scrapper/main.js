d952786d4e8069a4b655e561fb6ab6cd451f06db
saket bharti <saket@tidalwave.tech>
2022-03-25 00:26:09 +0530

    table print of innings

--- Node/webScrapping/espn_scrapper/main.js
+++ Node/webScrapping/espn_scrapper/main.js
@@ -38,4 +38,6 @@ function handleHTML(html) {
     allmatchobj.getAllMatch(fullLink);
 
     
+
+    
 }
--- Node/webScrapping/espn_scrapper/scorecard.js
+++ Node/webScrapping/espn_scrapper/scorecard.js
@@ -44,6 +44,62 @@ function getMatchDetail(html) {
 
       console.log(matchRes.text());
 
+    
+
+        let teamNames= selecTool(".name-detail>.name-link");
+
+        let team1 = selecTool(teamNames[0]).text();
+        let team2 = selecTool(teamNames[1]).text();
+
+        console.log(team1);
+        console.log(team2);
+
+       // let allBatsmenRows = selecTool(".table.batsman tbody >tr");
+        //console.log(allBatsmenRows.text());
+
+        //5. get inning
+
+        let allBatsmenTable = selecTool(".table.batsman tbody");
+
+        console.log("number of  batmen  tables ->" , allBatsmenTable.length);
+
+
+        let htmlstring = "";
+
+        let count=0;
+
+        for( let i=0; i<allBatsmenTable.length; i++)
+        {
+            htmlstring+= selecTool(allBatsmenTable[i]).html();
+
+            let allRows = selecTool(allBatsmenTable[i]).find("tr");
+            
+            for( let i=0; i<allRows.length; i++)
+            {
+                let row = selecTool(allRows[i]);
+                let firstcolmnOfRow = row.find("td")[0];
+
+                if( selecTool(firstcolmnOfRow).hasClass("batsman-cell"))
+                    {
+                        let playername =  selecTool(row.find("td")[0]).text();
+                        let runs        = selecTool(row.find("td")[2]).text();
+                        let balls       = selecTool(row.find("td")[3]).text();
+                        let fours       = selecTool(row.find("td")[5]).text();
+                        let sixes       = selecTool(row.find("td")[6]).text();
+                        let SR          = selecTool(row.find("td")[7]).text();
+
+                        console.log(`playername-> ${playername} \t runs -> ${runs} \t balls -> ${balls}  \t 4s -> ${fours} \t 6s-> ${sixes}  \t strike Rate -> ${SR}`);
+
+                    }
+            }
+            
+        }
+        //console.log(htmlstring);
+
+
+
+
+
 
 
     
