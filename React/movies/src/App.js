bce5f930ff20281d0b7e78faa1d59bc1ccea51d7
saket bharti <saket@tidalwave.tech>
2022-07-06 08:53:27 +0530

    :todo: movies appp started

---
+++ React/movies/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Banner from './Components/Banner';
+
+function App() {
+  return (
+    
+    <>
+    <Navbar/>
+    <Banner/>
+    </>
+  );
+}
+
+export default App;
